b4cd600e1bbfd074c15c3c138faa10c13bc3a57a
Dan P <73758508+benochi@users.noreply.github.com>
2021-11-15 21:06:09 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const app = express();
+const ExpressError = require('./expressError');
+
+//import functions from helpers.js
+const { handleNumsArray, findMode, findMean, findMedian } = require('./helpers');
+
+app.get('/mean', function(req, res, next) {
+  if (!req.query.nums) {
+    throw new ExpressError('A comma-separated list of numbers is required.', 400)
+  }
+  let numsAsStrings = req.query.nums.split(',');
+  // check if anything bad was put in
+  let nums = handleNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
+
+  let result = {
+    operation: "mean",
+    result: findMean(nums)
+  }
+  return res.send(result);
+});
+
+app.get('/median', function(req, res, next) {
+  if (!req.query.nums) {
+    throw new ExpressError('A comma-separated list of numbers is required.', 400)
+  }
+  let numsAsStrings = req.query.nums.split(',');
+  // check if anything bad was put in
+  let nums = handleNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
+
+  let result = {
+    operation: "median",
+    result: findMedian(nums)
+  }
+  return res.send(result);  
+});
+
+app.get('/mode', function(req, res, next) {
+  if (!req.query.nums) {
+    throw new ExpressError('A comma-separated list of numbers is required.', 400)
+  }
+  let numsAsStrings = req.query.nums.split(',');
+  // check if anything bad was put in
+  let nums = handleNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
+
+  let result = {
+    operation: "mode",
+    result: findMode(nums)
+  }
+
+  return res.send(result);
+
+ 
+});
+
+/** general error handler */
+
+app.use(function (req, res, next) {
+  const err = new ExpressError("Not Found",404);
+  // pass the error to the next piece of middleware
+  return next(err);
+});
+
+/** general error handler */
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err,
+    message: err.message
+  });
+});
+
+
+app.listen(3000, function() {
+  console.log(`Server starting on port 3000`);
+});
